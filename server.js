6357e28b6e79752bdbd07637c966dbb80053cf76
Jaehwa <Jaehwa.jh@gmail.com>
2023-07-31 15:54:14 -0700

    added react, nextjs

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ require("./config/passport")(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '../public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
